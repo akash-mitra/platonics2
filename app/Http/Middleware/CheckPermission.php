d900902e28c4aaae3a3806dc7df2df9612edb53d
Akash Mitra <akashmitra@gmail.com>
2018-06-07 16:19:14 +0800

    Changed cache remember to rememberForever in checkAcl

--- app/Http/Middleware/CheckPermission.php
+++ app/Http/Middleware/CheckPermission.php
@@ -2,57 +2,58 @@
 
 namespace App\Http\Middleware;
 
+use DB;
+use Auth;
 use Closure;
-
+use App\Permission;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Cache;
-use Auth;
-use DB;
-use App\Permission;
 
 class CheckPermission
 {
-    private $resource;
-    private $action;
     private $type;
+    private $action;
+    private $resource;
 
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure                 $next
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
-        $method = explode(".", Route::currentRouteName());
-        $this->resource = $method[0] == 'admin' ? $method[1] : $method[0];
+        $method = explode('.', Route::currentRouteName());
+
+        $this->type = Auth::guest() ? 'Visitor' : Auth::user()->type;
+
         $this->action = $method[0] == 'admin' ? $method[0] : $method[1];
-        $this->type = Auth::guest() ? 'Visitor' : Auth::user()->type; 
-        
-        if($this->checkAcl())
+
+        $this->resource = $method[0] == 'admin' ? $method[1] : $method[0];
+
+        if ($this->checkAcl()) {
             return $next($request);
-        else {
-            if($request->ajax())
-                return response()->json(['status' => 'You do not have permission'], 302);
-            else
-                return back()->with('status', 'You do not have permission');
         }
-        
 
+        if ($request->ajax()) {
+            return response()->json(['status' => 'You do not have permission'], 302);
+        } else {
+            return back()->with('status', 'You do not have permission');
+        }
     }
-
 
     /**
      * Check allow in AccessControlList.
      */
     private function checkAcl()
     {
-        //$acl = Permission::all();
-        $acl = Cache::remember('permissions', 60, function() {
+        $acl = Cache::rememberForever('permissions', function () {
             return DB::table('permissions')->get();
         });
+
         $permission = $acl->where('type', $this->type)->where('resource', $this->resource)->where('action', $this->action)->pluck('permission')->first();
-        return $permission == "allow" ? true : false;
+
+        return $permission == 'allow' ? true : false;
     }
 }
